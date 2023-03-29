3b1aff270b5d32aef5faadac083f3d97a9520cd1
dev-com2020 <kwp@tlen.pl>
2023-03-29 14:43:57 +0200

    14:43

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
     { path: 'products', component: ProductListComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
     { path: '', redirectTo: 'products', pathMatch: 'full' }
     
     
